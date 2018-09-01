bc0e34f3a4451aca636c5fe4a4202382423fbf03
JobGetabu <getabujob@gmail.com>
2018-09-01 20:20:13 +0300

    repackaged app & added firebase

--- app/src/main/java/app/hacela/chamatablebanking/ui/MainActivity.java
+++ app/src/main/java/app/hacela/chamatablebanking/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package app.hacela.chamatablebanking;
+package app.hacela.chamatablebanking.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import app.hacela.chamatablebanking.R;
 
 public class MainActivity extends AppCompatActivity {
 
