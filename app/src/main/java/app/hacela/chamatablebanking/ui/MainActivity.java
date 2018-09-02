c124d8bfec8bd9e2b801e9fb6151aaa02e76d835
JobGetabu <getabujob@gmail.com>
2018-09-02 16:57:32 +0300

    initial dashboard designed

--- app/src/main/java/app/hacela/chamatablebanking/ui/MainActivity.java
+++ app/src/main/java/app/hacela/chamatablebanking/ui/MainActivity.java
@@ -1,9 +1,10 @@
 package app.hacela.chamatablebanking.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import app.hacela.chamatablebanking.R;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
     }
+
 }
