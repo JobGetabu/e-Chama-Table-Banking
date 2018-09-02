9edd98788afb6a9d2d9a6f7d6df55dad989faf9b
JobGetabu <getabujob@gmail.com>
2018-09-02 23:22:30 +0300

    drop down group transactions works - UI

--- app/src/main/java/app/hacela/chamatablebanking/ui/MainActivity.java
+++ app/src/main/java/app/hacela/chamatablebanking/ui/MainActivity.java
@@ -1,12 +1,23 @@
 package app.hacela.chamatablebanking.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.diegodobelo.expandingview.ExpandingItem;
+import com.diegodobelo.expandingview.ExpandingList;
 
 import app.hacela.chamatablebanking.R;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainA";
+    private ExpandingList mExpandingList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +25,96 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        mExpandingList = findViewById(R.id.expanding_list_main);
+        createItems();
 
+    }
+
+    private void createItems() {
+        addItem("Contribution", new String[]{"House", "Boat", "Candy", "Collection", "Sport", "Ball", "Head"}, R.color.pink, R.drawable.group_bg);
+        addItem("Loans", new String[]{"Dog", "Horse", "Boat"}, R.color.greenb, R.drawable.loan_bg);
+        addItem("Fines", new String[]{"Cat"}, R.color.greenb3, R.drawable.profit_bg);
+        addItem("Projects", new String[]{"Parrot", "Elephant", "Coffee"}, R.color.purpleb, R.drawable.proj_bg);
+
+    }
+
+    private void addItem(String title, String[] subItems, int colorRes, int iconRes) {
+        //Let's create an item with R.layout.expanding_layout
+        final ExpandingItem item = mExpandingList.createNewItem(R.layout.expanding_layout);
+
+        //If item creation is successful, let's configure it
+        if (item != null) {
+            item.setIndicatorColorRes(colorRes);
+            item.setIndicatorIconRes(iconRes);
+            //It is possible to get any view inside the inflated layout. Let's set the text in the item
+            ((TextView) item.findViewById(R.id.e_g_item)).setText(title);
+
+            //We can create items in batch.
+            item.createSubItems(subItems.length);
+            for (int i = 0; i < item.getSubItemsCount(); i++) {
+                //Let's get the created sub item by its index
+                final View view = item.getSubItemView(i);
+
+                //Let's set some values in
+                configureSubItem(item, view, subItems[i]);
+            }
+
+
+           /*
+            item.findViewById(R.id.add_more_sub_items).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showInsertDialog(new OnItemCreated() {
+                        @Override
+                        public void itemCreated(String title) {
+                            View newSubItem = item.createSubItem();
+                            configureSubItem(item, newSubItem, title);
+                        }
+                    });
+                }
+            });
+
+
+            item.findViewById(R.id.remove_item).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mExpandingList.removeItem(item);
+                }
+            });
+            */
+        }
+    }
+
+    private void configureSubItem(final ExpandingItem item, final View view, String subTitle) {
+        ((TextView) view.findViewById(R.id.e_gs_details)).setText(subTitle);
+        ((TextView) view.findViewById(R.id.e_gs_date)).setText("12/08/2018");
+        ((TextView) view.findViewById(R.id.e_gs_money)).setText("+ Ksh 2,000");
+
+       /* view.findViewById(R.id.remove_sub_item).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                item.removeSubItem(view);
+            }
+        });*/
+    }
+
+    private void showInsertDialog(final OnItemCreated positive) {
+        final EditText text = new EditText(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(text);
+        builder.setTitle(R.string.enter_title);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                positive.itemCreated(text.getText().toString());
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.show();
+    }
+
+    interface OnItemCreated {
+        void itemCreated(String title);
     }
 
 }
