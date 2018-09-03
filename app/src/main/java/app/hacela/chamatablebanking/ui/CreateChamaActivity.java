ecd2aeec1893ff2a89f4c69ede34b4f5d4cf86ea
JobGetabu <getabujob@gmail.com>
2018-09-03 23:24:09 +0300

    added activity create chama

---
+++ app/src/main/java/app/hacela/chamatablebanking/ui/CreateChamaActivity.java
@@ -0,0 +1,15 @@
+package app.hacela.chamatablebanking.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import app.hacela.chamatablebanking.R;
+
+public class CreateChamaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_chama);
+    }
+}
