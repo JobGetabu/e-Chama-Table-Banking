df68372fea74d5f20b10c806f022a88fd6ea68a5
JobGetabu <getabujob@gmail.com>
2018-09-05 16:06:41 +0300

    constants utils

---
+++ app/src/main/java/app/hacela/chamatablebanking/util/Constants.java
@@ -0,0 +1,13 @@
+package app.hacela.chamatablebanking.util;
+
+/**
+ * Created by Job on Wednesday : 9/5/2018.
+ */
+public class Constants {
+    public static final String USERCOL = "Users";
+    public static final String GROUPSCOL = "Groups";
+    public static final String GROUPSACCOUNT = "GroupsAccount";
+    public static final String GROUPSCONTRIBUTIONDEFAULT = "GroupsContributionDefault";
+    public static final String GROUPSMEMBERS = "GroupsMembers";
+    public static final String USERSTRANSACTIONS = "UsersTransactions";
+}
