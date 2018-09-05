af42c192b7af1fe69e06796381be7ba278c75a30
JobGetabu <getabujob@gmail.com>
2018-09-05 15:53:34 +0300

    groups account pojo

---
+++ app/src/main/java/app/hacela/chamatablebanking/datasource/GroupsAccount.java
@@ -0,0 +1,41 @@
+package app.hacela.chamatablebanking.datasource;
+
+/**
+ * Created by Job on Wednesday : 9/5/2018.
+ */
+public class GroupsAccount {
+    private double amount;
+    private double divident;
+
+    public GroupsAccount() {
+    }
+
+    public GroupsAccount(double amount, double divident) {
+        this.amount = amount;
+        this.divident = divident;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupsAccount{" +
+                "amount=" + amount +
+                ", divident=" + divident +
+                '}';
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getDivident() {
+        return divident;
+    }
+
+    public void setDivident(double divident) {
+        this.divident = divident;
+    }
+}
