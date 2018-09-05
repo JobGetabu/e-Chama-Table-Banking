5a3bb845f395df4fc261db6c2eab77c0cece8e3a
JobGetabu <getabujob@gmail.com>
2018-09-05 15:51:26 +0300

    users pojo

---
+++ app/src/main/java/app/hacela/chamatablebanking/datasource/Users.java
@@ -0,0 +1,53 @@
+package app.hacela.chamatablebanking.datasource;
+
+/**
+ * Created by Job on Wednesday : 9/5/2018.
+ */
+public class Users {
+    private String username;
+    private String devicetoken;
+    private String photourl;
+
+    public Users() {
+    }
+
+    public Users(String username, String devicetoken, String photourl) {
+        this.username = username;
+        this.devicetoken = devicetoken;
+        this.photourl = photourl;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "username='" + username + '\'' +
+                ", devicetoken='" + devicetoken + '\'' +
+                ", photourl='" + photourl + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDevicetoken() {
+        return devicetoken;
+    }
+
+    public void setDevicetoken(String devicetoken) {
+        this.devicetoken = devicetoken;
+    }
+
+    public String getPhotourl() {
+        return photourl;
+    }
+
+    public void setPhotourl(String photourl) {
+        this.photourl = photourl;
+    }
+}
+
