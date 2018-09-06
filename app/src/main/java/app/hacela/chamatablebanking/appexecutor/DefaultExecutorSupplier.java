555cf0ea79d85f99687272beb6032236bf54d626
JobGetabu <getabujob@gmail.com>
2018-09-06 21:58:10 +0300

    added priority Thread Factory

--- app/src/main/java/app/hacela/chamatablebanking/appexecutor/DefaultExecutorSupplier.java
+++ app/src/main/java/app/hacela/chamatablebanking/appexecutor/DefaultExecutorSupplier.java
@@ -1,6 +1,12 @@
 package app.hacela.chamatablebanking.appexecutor;
 
+import android.os.Process;
+
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Job on Tuesday : 4/24/2018.
@@ -18,6 +24,10 @@ public class DefaultExecutorSupplier {
      * thread pool executor for main thread tasks
      */
     private final Executor mMainThreadExecutor;
+    /*
+     * thread pool executor for background tasks
+     */
+    private final ThreadPoolExecutor mForBackgroundTasks;
     /*
      * an instance of DefaultExecutorSupplier
      */
@@ -41,9 +51,23 @@ public class DefaultExecutorSupplier {
     private DefaultExecutorSupplier() {
 
 
+        // setting the thread factory
+        ThreadFactory backgroundPriorityThreadFactory = new
+                PriorityThreadFactory(Process.THREAD_PRIORITY_BACKGROUND);
 
         // setting the thread pool executor for mMainThreadExecutor;
         mMainThreadExecutor = new MainThreadExecutor();
+
+        // setting the thread pool executor for mForBackgroundTasks;
+        mForBackgroundTasks = new ThreadPoolExecutor(
+                NUMBER_OF_CORES * 2,
+                NUMBER_OF_CORES * 2,
+                60L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(),
+                backgroundPriorityThreadFactory
+        );
+
     }
 
     /*
@@ -51,5 +75,12 @@ public class DefaultExecutorSupplier {
      */
     public Executor forMainThreadTasks() {
         return mMainThreadExecutor;
+    }
+
+    /*
+     * returns the thread pool executor for background task
+     */
+    public ThreadPoolExecutor forBackgroundTasks() {
+        return mForBackgroundTasks;
     }
 }
--- app/src/main/java/app/hacela/chamatablebanking/viewmodel/MainViewModel.java
+++ app/src/main/java/app/hacela/chamatablebanking/viewmodel/MainViewModel.java
@@ -2,12 +2,23 @@ package app.hacela.chamatablebanking.viewmodel;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import app.hacela.chamatablebanking.datasource.Users;
+import app.hacela.chamatablebanking.repository.FirebaseDocumentLiveData;
+
+import static app.hacela.chamatablebanking.util.Constants.GROUPSMEMBERSCOL;
+import static app.hacela.chamatablebanking.util.Constants.USERCOL;
 
 /**
  * Created by Job on Tuesday : 9/4/2018.
@@ -16,12 +27,59 @@ public class MainViewModel extends AndroidViewModel {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore mFirestore;
+    private String currentUserId;
+
+    public static final String TAG = "AccountVM";
+
+    //few db references
+    private DocumentReference userRef;
+    private DocumentReference userMemberRef;
+    private DocumentReference groupRef;
+    private DocumentReference groupAccountRef;
+
+    //live datas
+    private FirebaseDocumentLiveData mUserLiveData;
+    private FirebaseDocumentLiveData mUserMemberLiveData;
+
+    //mediators
+    private MediatorLiveData<Users> usersMediatorLiveData = new MediatorLiveData<>();
 
     public MainViewModel(@NonNull Application application,FirebaseAuth mAuth,FirebaseFirestore mFirestore) {
         super(application);
 
         this.mAuth = mAuth;
         this.mFirestore = mFirestore;
+        this.currentUserId = mAuth.getCurrentUser().getUid();
+
+        //init db refs 
+        userRef = mFirestore.collection(USERCOL).document(currentUserId);
+        userMemberRef = mFirestore.collection(GROUPSMEMBERSCOL).document(currentUserId);
+
+
+        //init livedatas
+        mUserLiveData = new FirebaseDocumentLiveData(userRef);
+        mUserMemberLiveData = new FirebaseDocumentLiveData(userMemberRef);
+
+        // Set up the MediatorLiveData to convert DataSnapshot objects into POJO objects
+        workOnUsersLiveData();
+    }
+
+    private void workOnUsersLiveData() {
+
+        usersMediatorLiveData.addSource(mUserLiveData, new Observer<DocumentSnapshot>() {
+            @Override
+            public void onChanged(@Nullable DocumentSnapshot documentSnapshot) {
+
+                if (documentSnapshot != null){
+
+
+
+                }else {
+                    usersMediatorLiveData.postValue(null);
+                }
+            }
+        });
+
     }
 
     /**
