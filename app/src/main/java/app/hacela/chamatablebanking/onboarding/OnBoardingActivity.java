a5114ea8b4a11609451a6c8e57eaee422d5fd7d9
abedyngash <abedyngash@gmail.com>
2018-10-24 13:02:09 +0300

    Added the OnBoarding dependency

---
+++ app/src/main/java/app/hacela/chamatablebanking/onboarding/OnBoardingActivity.java
@@ -0,0 +1,38 @@
+package app.hacela.chamatablebanking.onboarding;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ramotion.paperonboarding.PaperOnboardingFragment;
+import com.ramotion.paperonboarding.PaperOnboardingPage;
+
+import java.util.ArrayList;
+
+import app.hacela.chamatablebanking.R;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+
+        PaperOnboardingPage scr1 = new PaperOnboardingPage("Hotels",
+                "All hotels and hostels are sorted by hospitality rating",
+                Color.parseColor("#678FB4"), R.drawable.ic_mortgage, R.drawable.ic_home);
+        PaperOnboardingPage scr2 = new PaperOnboardingPage("Banks",
+                "We carefully verify all banks before add them into the app",
+                Color.parseColor("#65B0B4"), R.drawable.ic_receivecash, R.drawable.ic_toolbar_arrow_up);
+        PaperOnboardingPage scr3 = new PaperOnboardingPage("Stores",
+                "All local stores are categorized for your convenience",
+                Color.parseColor("#9B90BC"), R.drawable.ic_add_pay, R.drawable.ic_action_add);
+
+        ArrayList<PaperOnboardingPage> elements = new ArrayList<>();
+        elements.add(scr1);
+        elements.add(scr2);
+        elements.add(scr3);
+
+        PaperOnboardingFragment onBoardingFragment = PaperOnboardingFragment.newInstance(elements);
+    }
+}
