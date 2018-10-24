cdf9ab8b1d018c846a5a606ab790213eae1486cb
abedyngash <abedyngash@gmail.com>
2018-10-24 14:38:39 +0300

    Created the login Activity and interface

---
+++ app/src/main/java/app/hacela/chamatablebanking/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package app.hacela.chamatablebanking.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.hacela.chamatablebanking.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
