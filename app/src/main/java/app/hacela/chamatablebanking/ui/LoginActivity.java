75246ca7ac5525acd0a5618b9f9aebc530589220
abedyngash <abedyngash@gmail.com>
2018-10-24 18:01:41 +0300

    Added login with email and password functionality. Next: Google Sign in

--- app/src/main/java/app/hacela/chamatablebanking/ui/LoginActivity.java
+++ app/src/main/java/app/hacela/chamatablebanking/ui/LoginActivity.java
@@ -1,5 +1,7 @@
 package app.hacela.chamatablebanking.ui;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -47,6 +53,36 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = loginEmail.getEditText().getText().toString().trim();
+                String password = loginPassword.getEditText().getText().toString().trim();
+
+                if (email.isEmpty() | password.isEmpty()){
+                    if (email.isEmpty()){
+                        loginEmail.getEditText().setError("Email is required");
+                        loginEmail.requestFocus();
+                    }
+                    if (password.isEmpty()){
+                        loginPassword.getEditText().setError("Password is required");
+                        loginPassword.requestFocus();
+                    }
+                }
+                else {
+                    userAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        Intent main = new Intent(LoginActivity.this, MainActivity.class);
+                                        main.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(main);
+                                    }
+                                    else {
+                                        Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
 
             }
         });
