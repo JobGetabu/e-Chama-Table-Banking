c9719d3e12ae99ca1b969e8ba58d2fc6a5a2dff8
abedyngash <abedyngash@gmail.com>
2018-10-26 18:55:26 +0300

    Working on the Folding cell

--- app/src/main/java/app/hacela/chamatablebanking/ui/GroupContributionsActivity.java
+++ app/src/main/java/app/hacela/chamatablebanking/ui/GroupContributionsActivity.java
@@ -1,15 +1,32 @@
 package app.hacela.chamatablebanking.ui;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.ramotion.foldingcell.FoldingCell;
 
 import app.hacela.chamatablebanking.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GroupContributionsActivity extends AppCompatActivity {
+    @BindView(R.id.folding_cell)
+    FoldingCell fc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_contributions);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.folding_cell)
+    public void onFoldClicked(){
+        fc.toggle(false);
     }
 }
