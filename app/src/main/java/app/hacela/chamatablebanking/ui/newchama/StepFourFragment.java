90af77809c5971db1a8869db1446662417445483
JobGetabu <getabujob@gmail.com>
2018-10-26 11:10:12 +0300

    frags half done logic

--- app/src/main/java/app/hacela/chamatablebanking/ui/newchama/StepFourFragment.java
+++ app/src/main/java/app/hacela/chamatablebanking/ui/newchama/StepFourFragment.java
@@ -1,15 +1,20 @@
 package app.hacela.chamatablebanking.ui.newchama;
 
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import app.hacela.chamatablebanking.R;
+import app.hacela.chamatablebanking.viewmodel.CreateChamaViewModel;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -31,7 +36,10 @@ public class StepFourFragment extends Fragment {
     TextView st4Back;
     @BindView(R.id.st_4_next)
     TextView st4Next;
+
     Unbinder unbinder;
+
+    private CreateChamaViewModel model;
 
     public StepFourFragment() {
         // Required empty public constructor
@@ -48,6 +56,48 @@ public class StepFourFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        model = ViewModelProviders.of(getActivity()).get(CreateChamaViewModel.class);
+
+        onInit();
+
+        st4Regular.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if (adapterView.getSelectedItemPosition() == 0) {
+                    st4Daymonth.setVisibility(View.VISIBLE);
+                    st4Dayweek.setVisibility(View.GONE);
+
+                } else {
+                    st4Daymonth.setVisibility(View.GONE);
+                    st4Dayweek.setVisibility(View.VISIBLE);
+                }
+                Toast.makeText(getContext(), adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    private void onInit() {
+        String sel = st4Regular.getSelectedItem().toString();
+        if (sel.equals("Once a Month")){
+            st4Daymonth.setVisibility(View.VISIBLE);
+            st4Dayweek.setVisibility(View.GONE);
+        }else {
+            st4Daymonth.setVisibility(View.GONE);
+            st4Dayweek.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
@@ -55,9 +105,13 @@ public class StepFourFragment extends Fragment {
 
     @OnClick(R.id.st_4_back)
     public void onSt4BackClicked() {
+        model.setCurrentStep(3);
     }
 
     @OnClick(R.id.st_4_next)
     public void onSt4NextClicked() {
+        model.setCurrentStep(5);
+
+        //TODO: Save this on the db
     }
 }
