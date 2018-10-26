9518a3e231e04476cafdb9860cb531c494871088
JobGetabu <getabujob@gmail.com>
2018-10-26 09:25:34 +0300

    added logic in new chama activity

--- app/src/main/java/app/hacela/chamatablebanking/ui/newchama/StepThreeFragment.java
+++ app/src/main/java/app/hacela/chamatablebanking/ui/newchama/StepThreeFragment.java
@@ -1,10 +1,10 @@
 package app.hacela.chamatablebanking.ui.newchama;
 
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.button.MaterialButton;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
