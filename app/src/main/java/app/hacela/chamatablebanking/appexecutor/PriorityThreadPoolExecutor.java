475b938771016e03916942ea386ce302c8bc5c58
JobGetabu <getabujob@gmail.com>
2018-10-26 11:07:51 +0300

    refactored priority

---
+++ app/src/main/java/app/hacela/chamatablebanking/appexecutor/PriorityThreadPoolExecutor.java
@@ -0,0 +1,51 @@
+package app.hacela.chamatablebanking.appexecutor;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Job on Tuesday : 4/24/2018.
+ */
+public class PriorityThreadPoolExecutor extends ThreadPoolExecutor {
+
+    public PriorityThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
+                                      TimeUnit unit, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit,new PriorityBlockingQueue<Runnable>(), threadFactory);
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        PriorityFutureTask futureTask = new PriorityFutureTask((PriorityRunnable) task);
+        execute(futureTask);
+        return futureTask;
+    }
+
+    private static final class PriorityFutureTask extends FutureTask<PriorityRunnable>
+            implements Comparable<PriorityFutureTask> {
+        private final PriorityRunnable priorityRunnable;
+
+        public PriorityFutureTask(PriorityRunnable priorityRunnable) {
+            super(priorityRunnable, null);
+            this.priorityRunnable = priorityRunnable;
+        }
+
+        /*
+         * compareTo() method is defined in interface java.lang.Comparable and it is used
+         * to implement natural sorting on java classes. natural sorting means the the sort
+         * order which naturally applies on object e.g. lexical order for String, numeric
+         * order for Integer or Sorting employee by there ID etc. most of the java core
+         * classes including String and Integer implements CompareTo() method and provide
+         * natural sorting.
+         */
+        @Override
+        public int compareTo(PriorityFutureTask other) {
+            Priority p1 = priorityRunnable.getPriority();
+            Priority p2 = other.priorityRunnable.getPriority();
+            return p2.ordinal() - p1.ordinal();
+        }
+    }
+}
