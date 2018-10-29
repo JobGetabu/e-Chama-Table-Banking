a5b3fceff6c988efa3f190ec058a2586dc767ef7
abedyngash <abedyngash@gmail.com>
2018-10-29 17:26:42 +0300

    Implemented the Dummy Test Model for the Projects RecyclerView

--- app/src/main/java/app/hacela/chamatablebanking/ui/ProjectsActivity.java
+++ app/src/main/java/app/hacela/chamatablebanking/ui/ProjectsActivity.java
@@ -2,18 +2,29 @@ package app.hacela.chamatablebanking.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.ramotion.foldingcell.FoldingCell;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import app.hacela.chamatablebanking.R;
+import app.hacela.chamatablebanking.adapter.DummyTest;
+import app.hacela.chamatablebanking.adapter.DummyTestAdapter;
 import butterknife.BindView;
 
 public class ProjectsActivity extends AppCompatActivity {
-    @BindView(R.id.folding_cell)
-    FoldingCell fc;
+
     @BindView(R.id.projects_toolbar)
     Toolbar projects_toolbar;
+    @BindView(R.id.projects_recycler)
+    RecyclerView projects_recycler;
+
+    private RecyclerView.Adapter adapter;
+    private List<DummyTest> dummyTests;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +34,22 @@ public class ProjectsActivity extends AppCompatActivity {
         setSupportActionBar(projects_toolbar);
         getSupportActionBar().setTitle("Group Projects");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        projects_recycler.setHasFixedSize(true);
+        projects_recycler.setLayoutManager(new LinearLayoutManager(this));
+        dummyTests = new ArrayList<>();
+
+        for (int i = 0; i <= 10; i++){
+            DummyTest dummyTest = new DummyTest(
+                    "Member" + i + 1,
+                    "070011223344",
+                    "1500",
+                    "500"
+            );
+
+            dummyTests.add(dummyTest);
+        }
+
+        adapter = new DummyTestAdapter(dummyTests, this);
+        projects_recycler.setAdapter(adapter);
     }
 }
