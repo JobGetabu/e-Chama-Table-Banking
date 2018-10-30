78623066f37ae6ac9077f7ef5f6ce6bc697f9585
JobGetabu <getabujob@gmail.com>
2018-10-30 12:45:53 +0300

    phone auth and assets

--- app/src/main/java/app/hacela/chamatablebanking/ui/auth/PhoneAuthActivity.java
+++ app/src/main/java/app/hacela/chamatablebanking/ui/auth/PhoneAuthActivity.java
@@ -1,18 +1,65 @@
 package app.hacela.chamatablebanking.ui.auth;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.firestore.Source;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import app.hacela.chamatablebanking.R;
+import app.hacela.chamatablebanking.model.Users;
+import app.hacela.chamatablebanking.ui.MainActivity;
+import app.hacela.chamatablebanking.util.AppStatus;
+import app.hacela.chamatablebanking.util.DoSnack;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+import static app.hacela.chamatablebanking.util.Constants.GROUPSCOL;
+import static app.hacela.chamatablebanking.util.Constants.GROUPSMEMBERSCOL;
+import static app.hacela.chamatablebanking.util.Constants.GROUP_ID_PREFS;
+import static app.hacela.chamatablebanking.util.Constants.GROUP_NAME_PREFS;
+import static app.hacela.chamatablebanking.util.Constants.GROUP_ROLE_PREFS;
+import static app.hacela.chamatablebanking.util.Constants.USERCOL;
 
 public class PhoneAuthActivity extends AppCompatActivity {
+
+    private static final String TAG = "phone";
+    public static final int RC_SIGN_IN = 1001;
 
     @BindView(R.id.ph_number)
     TextInputLayout phNumber;
@@ -23,11 +70,37 @@ public class PhoneAuthActivity extends AppCompatActivity {
     @BindView(R.id.ph_google)
     ImageView phGoogle;
 
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
+    private GoogleSignInClient mGoogleSignInClient;
+    private DoSnack doSnack;
+    private SharedPreferences.Editor sharedPreferencesEditor;
+    private SharedPreferences mSharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_auth);
         ButterKnife.bind(this);
+
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+
+        doSnack = new DoSnack(this, PhoneAuthActivity.this);
+        sharedPreferencesEditor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
     private boolean isValidMobile(String phone) {
@@ -40,9 +113,250 @@ public class PhoneAuthActivity extends AppCompatActivity {
 
     @OnClick(R.id.ph_continue)
     public void onPhContinueClicked() {
+        if (validatePhone()){
+
+        }
     }
 
     @OnClick(R.id.ph_google)
     public void onPhGoogleClicked() {
+        if (!AppStatus.getInstance(getApplicationContext()).isOnline()) {
+
+            doSnack.showSnackbar("You're offline", "Retry", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onPhGoogleClicked();
+                }
+            });
+
+            return;
+        }
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private boolean validatePhone() {
+        boolean valid = true;
+
+        String phone = phNumber.getEditText().getText().toString();
+
+        if (phone.isEmpty() || !isValidMobile(phone)) {
+            phNumber.setError("enter valid phone number");
+            valid = false;
+        } else {
+            phNumber.setError(null);
+        }
+
+        return valid;
+    }
+
+    private boolean validateCode() {
+        boolean valid = true;
+
+        String code = phCode.getEditText().getText().toString();
+
+        //TODO: verify sent code too
+
+        if (code.isEmpty()) {
+            phCode.setError("invalid code");
+            valid = false;
+        } else {
+            phCode.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#FF4081"));
+        pDialog.setTitleText("Logging in...");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            final FirebaseUser user = mAuth.getCurrentUser();
+                            final String device_token = FirebaseInstanceId.getInstance().getToken();
+                            final String mCurrentUserid = mAuth.getCurrentUser().getUid();
+
+                            // refactor this not to write to DB each time...check if account exists
+
+                            DocumentReference docReference = mFirestore.collection(USERCOL).document(mCurrentUserid);
+                            docReference.get()
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                DocumentSnapshot document = task.getResult();
+                                                if (document.exists()) {
+                                                    Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+
+                                                    Map<String, Object> updateTokenMap = new HashMap<>();
+                                                    updateTokenMap.put("devicetoken", device_token);
+
+                                                    //update token only
+                                                    updateTokenOnly(mCurrentUserid, pDialog, updateTokenMap);
+
+                                                } else {
+                                                    Log.d(TAG, "No such document");
+
+                                                    //logging in with no pre account
+                                                    //region create fresh account
+
+                                                    pDialog.changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                                                    pDialog.setTitleText("Account doesn't exists! \n Creating one...");
+
+                                                    //write to db
+                                                    writingToUsers(pDialog, device_token, user, mCurrentUserid);
+
+                                                    //endregion
+                                                }
+                                            } else {
+                                                Log.d(TAG, "get failed with ", task.getException());
+                                            }
+                                        }
+                                    });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            pDialog.dismissWithAnimation();
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(android.R.id.content), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void writingToUsers(final SweetAlertDialog pDialog, String device_token, FirebaseUser user, String mCurrentUserid) {
+
+        // Set the value of 'Users'
+        DocumentReference usersRef = mFirestore.collection(USERCOL).document(mCurrentUserid);
+
+        String usern = user.getDisplayName();
+        String pp = user.getPhotoUrl().toString();
+
+        Users users = new Users(usern, device_token, pp);
+        usersRef.set(users)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        pDialog.dismissWithAnimation();
+                        sendUserToMainActivity();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pDialog.dismiss();
+                doSnack.errorPrompt("Oops...", e.getMessage());
+            }
+        });
+    }
+
+    private void updateTokenOnly(String mCurrentUserid,
+                                 final SweetAlertDialog pDialog, Map<String, Object> updateTokenMap) {
+
+        loadYourGroup();
+
+        // Set the value of 'Users'
+        DocumentReference usersRef = mFirestore.collection(USERCOL).document(mCurrentUserid);
+
+        usersRef.set(updateTokenMap, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        pDialog.dismissWithAnimation();
+                        sendUserToMainActivity();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pDialog.dismiss();
+                doSnack.errorPrompt("Oops...", e.getMessage());
+            }
+        });
+    }
+
+    private void sendUserToMainActivity() {
+        Intent main = new Intent(this, MainActivity.class);
+        main.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(main);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+                Snackbar.make(findViewById(android.R.id.content), "Google sign in failed", Snackbar.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    private void loadYourGroup() {
+
+        mFirestore.collection(GROUPSMEMBERSCOL).document(mAuth.getCurrentUser().getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull final Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+
+                            if (document.exists()) {
+                                //chama exists
+                                mFirestore.collection(GROUPSMEMBERSCOL).document(mAuth.getCurrentUser().getUid())
+                                        .get()
+                                        .addOnSuccessListener(PhoneAuthActivity.this, new OnSuccessListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                                                String mGroupId = documentSnapshot.getString("groupid");
+                                                String mUserRole = documentSnapshot.getString("userrole");
+                                                findGroupName(mGroupId);
+
+                                                sharedPreferencesEditor.putString(GROUP_ID_PREFS, mGroupId);
+                                                sharedPreferencesEditor.putString(GROUP_ROLE_PREFS, mUserRole);
+                                                sharedPreferencesEditor.apply();
+                                            }
+                                        });
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void findGroupName(String mGroupId) {
+        mFirestore.collection(GROUPSCOL).document(mGroupId)
+                .get(Source.DEFAULT)
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            sharedPreferencesEditor.putString(GROUP_NAME_PREFS, task.getResult().getString("groupname"));
+                            sharedPreferencesEditor.apply();
+                        }
+                    }
+                });
     }
 }
