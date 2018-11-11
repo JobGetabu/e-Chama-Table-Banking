11600babc772d1acf66ad729e2fc27fbeac24f52
abedyngash <abedyngash@gmail.com>
2018-11-12 02:19:12 +0300

    Added the Group Chat Activity

---
+++ app/src/main/java/app/hacela/chamatablebanking/ui/chat/GroupChatActivity.java
@@ -0,0 +1,15 @@
+package app.hacela.chamatablebanking.ui.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.hacela.chamatablebanking.R;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+    }
+}
